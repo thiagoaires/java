264d88834df7b01b87a7cd210e734c332002c353
Thiago Aires da Silva <makarrao@gmail.com>
2019-06-03 19:52:47 -0300

    Java / Capitulo 1 / estudei por cima, pra pegar os conceitos basicos da aula

---
+++ Java 1/TestaConversao.java
@@ -0,0 +1,7 @@
+public class TestaConversao {
+  public static void main(String[] args) {
+    double salario = 1556.65;
+    int valor = (int) salario;
+    System.out.println(valor);
+  }
+}
---
+++ Java 1/TestaValores.java
@@ -0,0 +1,8 @@
+public class TestaValores {
+  public static void main(String[] args) {
+    int primeiro = 4;
+    int segundo = 332;
+
+    System.out.println(primeiro + segundo);
+  }
+}
