70e90cd6671d242c531bfdc7689dab8870330f66
Thiago Aires <makarrao@gmail.com>
2019-06-23 19:20:00 -0300

    Android Completo / Loops

---
+++ JavaParaAndroid/src/DoWhileFor.java
@@ -0,0 +1,15 @@
+public class DoWhileFor {
+    public static void main(String[] args) {
+
+        int numero = 1;
+
+//        do{
+//            System.out.println(numero);
+//            numero++;
+//        } while ( numero < 5);
+
+        for(int n = 0; n < 5; n++){
+            System.out.println(n);
+        }
+    }
+}
---
+++ JavaParaAndroid/src/Loops.java
@@ -0,0 +1,11 @@
+public class Loops {
+    public static void main(String[] args) {
+
+        String[] postagens = {"lpi","hadsihaisdohads", "adsda213123122133", "asddadsadsadas"};
+        int numero = 0;
+        while(numero < postagens.length){
+            System.out.println(postagens[numero]);
+            numero++;
+        }
+    }
+}
